41b999334b54b956264b2f47013f01a2e93f79bd
Shivansh2203 <168472217+Shivansh2203@users.noreply.github.com>
2025-04-04 16:24:53 +0530

    Updated navbar to close on nav link click

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,11 @@ import { RiCloseLine, RiMenu2Line } from "@remixicon/react";
 const Navbar = () => {
   const [menu, openMenu] = useState(false);
   const [showMenu, setShowmenu] = useState(true);
+  const toggleMenu = () => {
+    openMenu(!menu);
+    setShowmenu(!showMenu);
+  };
+
   return (
   
     <nav style={{ background: "#171d3254", backdropFilter: "blur(5px)" }} className="flex flex-wrap justify-between md:items-center text-white px-20 pt-6 md:px-21 bg-black bg-opacity-40 backdrop-blur-lg shadow-lg round-lg p-4 fixed top-0 w-full z-50">
@@ -13,18 +18,18 @@ const Navbar = () => {
           menu ? "block" : "hidden"
         }     mx-24 py-2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl bg-opacity-30 md:border-none text-center md:bg-transparent md:static md:mx-0 md:flex gap-6 `}
       >
-        <a href="#About">
+        <a href="#About" onClick={toggleMenu}>
           <li className="text-2xl transition-all duration-300 p-1 md:p-0 hover:text-yellow-500">
             About
           </li>
         </a>
        
-        <a href="#Projects">
+        <a href="#Projects" onClick={toggleMenu}>
           <li className="text-2xl transition-all duration-300 p-1 md:p-0  hover:text-yellow-500">
             Projects
           </li>
         </a>
-        <a href="#Footer">
+        <a href="#Footer" onClick={toggleMenu}>
           <li className="text-2xl transition-all duration-300 p-1 md:p-0  hover:text-yellow-500">
             Contact
           </li>
@@ -43,6 +48,7 @@ const Navbar = () => {
         <RiCloseLine
           size={30}
           className="md:hidden absolute right-10 top-6 transition-all duration-300"
+          onClick={toggleMenu} 
         />
       )}
     </nav>
